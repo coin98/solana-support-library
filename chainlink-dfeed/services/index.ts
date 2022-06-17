4ccd916016f7d64ecf144ac6eb3a684d5720d9d8
Tung Nguyen <87560224+tungnguyennhat98@users.noreply.github.com>
2022-06-17 14:54:37 +0700

    release 0.1.0

---
+++ chainlink-dfeed/services/index.ts
@@ -0,0 +1,17 @@
+export {
+  ChainlinkDfeedService
+} from './chainlink_dfeed.service'
+export {
+  ChainkDfeedInstructionService,
+  Feed,
+  FeedDetail,
+  Round,
+  ScopeAggregator,
+  ScopeDecimals,
+  ScopeDescription,
+  ScopeLatestRoundData,
+  ScopeRoundData,
+  ScopeRoundDataDetail,
+  ScopeVersion
+} from './chainlink_dfeed_instruction.service'
+
